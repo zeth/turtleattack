03fc37f92ac0026b85d342126c74e526cda4363c
zeth <theology@gmail.com>
2013-09-29 09:27:54 +0100

    Added __init__.py file.

---
+++ turtleattack/__init__.py
@@ -0,0 +1,13 @@
+"""
+Turtle Attack - Defend the world from evil turtles.
+
+Evil turtles have walked out of the sea, the last hope for planet earth is a
+heroic spider.
+
+"""
+
+# The __init__.py file makes Python treat the turtleattack directory as a
+# package, this is called an initialization file.
+
+# For more information, read:
+# http://docs.python.org/3/tutorial/modules.html#packages
