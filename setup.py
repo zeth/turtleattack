789b64a24ca7a041ba2619b648efb05eff09f9ad
zeth <theology@gmail.com>
2013-10-02 20:02:39 +0100

    Minor change to setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='turtleattack',
       keywords = "turtle education kids",
       scripts=['tattack'],
       packages=['turtleattack'],
-      package_data={'turtleattack': ['images/*.gif', 'images/fire/*.gif']},
+      package_data={'turtleattack': ['images/*.gif',]},
       classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
